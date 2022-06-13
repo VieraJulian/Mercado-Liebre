753c7e5a10e21b2c166e6169b8d71f5491d0712f
VieraJulian <julianviera2019@hotmail.com>
2022-06-13 19:34:56 -0300

    Subiendo mercado liebre

---
+++ source/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3001, () => console.log("Servidor corriendo en el puerto 3000"));
+
+const public = path.resolve(__dirname, '../public');
+
+app.use(express.static(public));
+
+app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'views/home.html')));
+
+app.get("/register", (req, res) => res.sendFile(path.resolve(__dirname, 'views/register.html')));
+
+app.get("/login", (req, res) => res.sendFile(path.resolve(__dirname, 'views/login.html')));
