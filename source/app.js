97c0bac958933817a056c8f95772f8ce409acc61
VieraJulian <julianviera2019@hotmail.com>
2022-06-14 17:06:00 -0300

    Mercado Liebre terminado

--- source/app.js
+++ source/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3001, () => console.log("Servidor corriendo en el puerto 3000"));
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 
 const public = path.resolve(__dirname, '../public');
 
